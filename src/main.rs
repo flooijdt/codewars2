25bad49b9013f076c2375e4e77676f47b10f3bfd
flooijdt <flooijdt@localhost.localdomain>
2022-11-14 14:37:24 -0300

    first

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use roots::find_roots_quadratic;
-use roots::Roots;
 
 fn max_ball(v0: i32) -> i32 {
     // your code
@@ -8,16 +7,20 @@ fn max_ball(v0: i32) -> i32 {
     let v: f32;
     let g: f32;
 
-    let vms: f32 = v0 * 10 / 36;
-    let h = vms;
-    let t = 1.0;
+    let vms: f32 = (v0 as f32) * 10.0 / 36.0;
+    let mut h = vms;
+    let t: f32;
     let g = 9.81;
 
-    h = vms * t - 0.5 * g * t * t;
+    h = vms * t * 0.1 - 0.5 * g * t * t * 0.01;
 
-    println!("{:?}", t);
+    println!("{:?}", h);
+    let roots = find_roots_quadratic(-0.005 * 9.81, 0.1 * vms, -h);
+    println!("{:?}", roots);
+    h as i32
 }
 
 fn main() {
-    max_ball(10);
+    max_ball(37);
+    max_ball(45);
 }
