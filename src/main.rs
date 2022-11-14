c95da4bdf0b62265151e6ea390f4b7dee49f6cf1
flooijdt <flooijdt@localhost.localdomain>
2022-11-14 10:19:15 -0300

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use roots::find_roots_quadratic;
+use roots::Roots;
+
+fn max_ball(v0: i32) -> i32 {
+    // your code
+    let h: f32;
+    let t: f32;
+    let v: f32;
+    let g: f32;
+
+    let vms: f32 = v0 * 10 / 36;
+    let h = vms;
+    let t = 1.0;
+    let g = 9.81;
+
+    h = vms * t - 0.5 * g * t * t;
+
+    println!("{:?}", t);
+}
+
 fn main() {
-    println!("Hello, world!");
+    max_ball(10);
 }
