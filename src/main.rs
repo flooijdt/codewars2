95988758a2dec28606cbd5931ec756456baec311
flooijdt <flooijdt@localhost.localdomain>
2022-11-16 17:20:14 -0300

    first

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-use roots::find_roots_quadratic;
-
-fn max_ball(v0: i32) -> i32 {
-    // your code
-    let h: f32;
-    let t: f32;
-    let v: f32;
-    let g: f32;
-
-    let vms: f32 = (v0 as f32) * 10.0 / 36.0;
-    let mut h = vms;
-    let t: f32;
-    let g = 9.81;
-
-    h = vms * t * 0.1 - 0.5 * g * t * t * 0.01;
-
-    println!("{:?}", h);
-    let roots = find_roots_quadratic(-0.005 * 9.81, 0.1 * vms, -h);
-    println!("{:?}", roots);
-    h as i32
+fn count_duplicates(text: &str) -> u32 {
+    // Your code goes here
+    let mut text_vec: Vec<char> = text.chars().collect();
+    for i in text_vec {
+        println!("{:?}", i);
+    }
+    32u32
 }
-
 fn main() {
-    max_ball(37);
-    max_ball(45);
+    count_duplicates("whareva");
 }
