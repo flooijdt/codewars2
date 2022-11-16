e81bfa41464cc629a5ab10374759a38588563fb0
flooijdt <flooijdt@localhost.localdomain>
2022-11-16 17:49:09 -0300

    first

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ fn count_duplicates(text: &str) -> u32 {
     // Your code goes here
     let mut text_vec: Vec<char> = text.chars().collect();
     for i in text_vec {
-        println!("{:?}", i);
+        text_vec.remove(;
     }
-    let mut leters: Vec<char> = "a suA mae".chars().collect();
+    let mut leters: Vec<char> = "a suA mae904%$".chars().collect();
+    leters.sort();
 
     println!("{:?}", &leters);
-    println!("{:?}", &leters.sort());
     32u32
 }
 fn main() {
