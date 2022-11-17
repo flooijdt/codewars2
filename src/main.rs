041316578cfe0e97707dfd6b6bbfceffc48afd69
flooijdt <flooijdt@localhost.localdomain>
2022-11-17 10:16:39 -0300

    first

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+
 fn count_duplicates(text: &str) -> u32 {
     // Your code goes here
 
-    let mut leters: Vec<char> = "a suA mae904%$".chars().collect();
-    leters.sort();
     let mut newlist: Vec<String> = Vec::new();
+    let mut leters: Vec<char> = text.chars().collect();
 
-    for i in leters.iter() {
+    for i in leters {
         newlist.push(i.to_lowercase().to_string());
     }
+    newlist.sort();
 
-    println!("{:?}", &newlist);
-    32u32
-}
-fn main() {
-    count_duplicates("whareva");
+    let mut letershash: HashMap<String, usize> = newlist.into_iter().counts();
+    let mut counter = 0;
+
+    for i in letershash {
+        if i.1 > 1 {
+            counter += 1;
+        }
+    }
+    counter
 }
